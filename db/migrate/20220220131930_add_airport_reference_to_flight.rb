cfffeb710d72e66a6aa9dd44f0f9cfac748dd235
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2022-02-20 16:27:26 +0000

    Database set up

---
+++ db/migrate/20220220131930_add_airport_reference_to_flight.rb
@@ -0,0 +1,7 @@
+class AddAirportReferenceToFlight < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :flights, :departure_airport, foreign_key: { to_table: :airports}
+    add_reference :flights, :arrival_airport, foreign_key: { to_table: :airports}
+
+  end
+end
