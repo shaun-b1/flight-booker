89f7983cf2a42440d83cc48be849943a7ead659f
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2022-02-22 20:13:16 +0000

    stuck here

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,18 @@
 class Flight < ApplicationRecord
     belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
     belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
+
+    def self.search(departure_airport, arrival_airport)
+        if search 
+            departure_airport = Airport.find_by(airport_code: departure_airport)
+            arrival_airport = Airport.find_by(airport_code: arrival_airport)
+            if departure_airport && arrival_airport
+                self.where(departure_airport_id: departure_airport).where(arrival_airport_id: arrival_airport)
+            else 
+                @flights = Flight.all
+            end 
+        else 
+            @flights = Flight.all
+        end
+    end
 end
